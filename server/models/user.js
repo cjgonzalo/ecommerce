041b8279adbaba7f7ba8a817c77779a17fec1065
cjgonzalo <gonicasanova.gc@gmail.com>
2021-11-30 11:10:35 -0300

    agrego rol (admin / no admin) al usuario

--- server/models/user.js
+++ server/models/user.js
@@ -6,7 +6,8 @@ const userSchema = new Schema({
     password: {type: String, required: true},
     email: {type: String, required: true},
     //paymentMethod: {type: ??},
-    kart: [{type: product, default: []}]
+    kart: [{type: product, default: []}],
+    admin: {type: Boolean, required: true}
 })
 
 module.exports = userSchema
