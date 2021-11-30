57c5cdd0bcff6169136a9a52733ee89c43695502
cjgonzalo <gonicasanova.gc@gmail.com>
2021-11-30 10:52:45 -0300

    separo vistas en carpetas

---
+++ client/src/views/404/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const notFound = () => {
+    return (
+        <>
+            <h1>No pudimos encontrar esta ruta</h1>
+            <a href="/">Volver al inicio</a>
+        </>
+    )
+}
+
+export default notFound
