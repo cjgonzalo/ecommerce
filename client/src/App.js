98e5ea8c595a5b6af61dd2cee13c8c1ebed96620
cjgonzalo <gonicasanova.gc@gmail.com>
2021-11-30 11:07:16 -0300

    nuevas rutas

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,13 @@ import {
   Link,
   NavLink
 } from "react-router-dom";
-import Header from "./components/Header"
-import Contact from "./views/contact"
-import Login from "./views/login"
+import Header from "./components/header/Header"
+import Contact from "./views/contact/contact"
+import Login from "./views/login/login"
 import Home from "./views/home/home"
+import About from "./views/about/about"
+import NotFound from "./views/404/404"
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
@@ -18,9 +21,17 @@ function App() {
         <Routes>
           <Route path = "/contacto" element={<Contact />} />
           <Route path = "/login" element={<Login/>} />
+          <Route path = "/about" element={<About/>} />
+          <Route path = "/productos/juegos" element={<About/>} />
+          <Route path = "/productos/auriculares" element={<About/>} />
+          <Route path = "/productos/microfonos" element={<About/>} />
+          <Route path = "/productos/gabinetes" element={<About/>} />
+          <Route path = "/productos/sillas-gamer" element={<About/>} />
           <Route path = "/" element={<Home/>} />
+          <Route path = "*" element={<NotFound />} />
         </Routes>
       </main>
+      <Footer />
     </Router>
   );
 }
