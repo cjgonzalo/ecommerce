973b944b9408026cddb33afa31d7225019beaf2c
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-01 17:00:52 -0300

    cambio de nombre a la pagina de productos

---
+++ client/src/views/productsPage/productsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const productsPage = ({type}) => {
+    return (
+        <div>
+            <h1>Products test view</h1>
+        </div>
+    )
+}
+
+export default productsPage
