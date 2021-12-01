2d69a5c127db5a22dbfbf52ed3ea85f88d4c2cbe
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-01 17:00:03 -0300

    cambio en el responsive de columnas

--- client/src/components/productsRow/ProductsRow.jsx
+++ client/src/components/productsRow/ProductsRow.jsx
@@ -8,16 +8,16 @@ const ProductsRow = ({title, url}) => {
         
         <div className="main-container">
             <div className="row">
-            <div className="col-12 col-sm-6 col-md-3 product">
+            <div className="col-12 col-sm-6 col-lg-3 product">
                 <Product title="titulo" price={1000} desc="description" image="https://via.placeholder.com/300" />
             </div>
-            <div className="col-12 col-sm-6 col-md-3 product">
+            <div className="col-12 col-sm-6 col-lg-3 product">
                 <Product title="titulo" price={2000} desc="description" image="https://via.placeholder.com/300" />
             </div>
-            <div className="col-12 col-sm-6 col-md-3 product">
+            <div className="col-12 col-sm-6 col-lg-3 product">
                 <Product title="titulo" price={3000} desc="description" image="https://via.placeholder.com/300" />
             </div>
-            <div className="col-12 col-sm-6 col-md-3 product">
+            <div className="col-12 col-sm-6 col-lg-3 product">
                 <Product title="titulo" price={4000} desc="description" image="https://via.placeholder.com/300" />
             </div>
             </div>
