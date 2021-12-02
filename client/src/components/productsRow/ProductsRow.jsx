04b85b744fb5906d32f3761942ef5eac685542a6
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-02 00:41:48 -0300

    cambios en las columnas

--- client/src/components/productsRow/ProductsRow.jsx
+++ client/src/components/productsRow/ProductsRow.jsx
@@ -4,25 +4,23 @@ import "./ProductsRow.css"
 
 const ProductsRow = ({title, url}) => {
     return (
-        <>
-        
-        <div className="main-container">
+        <section className="products-row-container">
+            <h1 className="title"><a href={`/${title}`}>{title}</a></h1>
             <div className="row">
-            <div className="col-12 col-sm-6 col-lg-3 product">
+            <div className="col-12 col-sm-6 col-lg-3 product d-flex justify-content-center">
                 <Product title="titulo" price={1000} desc="description" image="https://via.placeholder.com/300" />
             </div>
-            <div className="col-12 col-sm-6 col-lg-3 product">
+            <div className="col-12 col-sm-6 col-lg-3 product d-flex justify-content-center">
                 <Product title="titulo" price={2000} desc="description" image="https://via.placeholder.com/300" />
             </div>
-            <div className="col-12 col-sm-6 col-lg-3 product">
+            <div className="col-12 col-sm-6 col-lg-3 product d-flex justify-content-center">
                 <Product title="titulo" price={3000} desc="description" image="https://via.placeholder.com/300" />
             </div>
-            <div className="col-12 col-sm-6 col-lg-3 product">
+            <div className="col-12 col-sm-6 col-lg-3 product d-flex justify-content-center">
                 <Product title="titulo" price={4000} desc="description" image="https://via.placeholder.com/300" />
             </div>
             </div>
-        </div>
-        </>
+        </section>
     )
 }
 
