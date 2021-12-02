3d90ee94d29907926b1fd4857a0b0f5e695f15a1
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-02 00:38:21 -0300

    carousel dentro de container

--- client/src/components/carousel/Carousel.jsx
+++ client/src/components/carousel/Carousel.jsx
@@ -10,48 +10,50 @@ const Carousel = () => {
 
     return (
         <>
-        <section class="carousel-container">
-            <div id="preview-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src={bannerJuegos} class="d-block w-100" alt="banner juegos"/>
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Juegos</h5>
-                            <a href="/productos/juegos" className="see-more">Ver más</a>
+        <div className="container">
+            <section class="carousel-container">
+                <div id="preview-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={bannerJuegos} class="d-block w-100" alt="banner juegos"/>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Juegos</h5>
+                                <a href="/productos/juegos" className="see-more">Ver más</a>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={bannerPerifericos} class="d-block w-100" alt="banner perisfericos"/>
+                            <div class="carousel-caption d-none d-md-block">
+                            <h5>Perifericos</h5>
+                                <a href="/productos/perifericos" className="see-more">Ver más</a>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={bannerGabinetes} class="d-block w-100" alt="banner gabinetes"/>
+                            <div class="carousel-caption d-none d-md-block">
+                            <h5>Gabinetes</h5>
+                                <a href="/productos/gabinetes" className="see-more">Ver más</a>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={bannerSillas} class="d-block w-100" alt="banner sillas gamer"/>
+                            <div class="carousel-caption d-none d-md-block">
+                            <h5>Sillas Gamer</h5>
+                                <a href="/productos/sillas-gamer" className="see-more">Ver más</a>
+                            </div>
                         </div>
                     </div>
-                    <div class="carousel-item">
-                        <img src={bannerPerifericos} class="d-block w-100" alt="banner perisfericos"/>
-                        <div class="carousel-caption d-none d-md-block">
-                        <h5>Perifericos</h5>
-                            <a href="/productos/perifericos" className="see-more">Ver más</a>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src={bannerGabinetes} class="d-block w-100" alt="banner gabinetes"/>
-                        <div class="carousel-caption d-none d-md-block">
-                        <h5>Gabinetes</h5>
-                            <a href="/productos/gabinetes" className="see-more">Ver más</a>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src={bannerSillas} class="d-block w-100" alt="banner sillas gamer"/>
-                        <div class="carousel-caption d-none d-md-block">
-                        <h5>Sillas Gamer</h5>
-                            <a href="/productos/sillas-gamer" className="see-more">Ver más</a>
-                        </div>
-                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#preview-carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#preview-carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#preview-carousel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#preview-carousel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </section>
+            </section>
+        </div>
         </>
     )
 }
