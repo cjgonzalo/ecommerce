98c830881f375930e9bd72f95561651908f6728c
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-02 00:44:32 -0300

    vista segun categoria

--- client/src/views/productsPage/productsPage.jsx
+++ client/src/views/productsPage/productsPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const productsPage = ({type}) => {
+
+    // hacer fetch a /api/"type" y mostrar los productos de ese tipo
+
     return (
         <div>
-            <h1>Products test view</h1>
+            <h1>{type} test view</h1>
         </div>
     )
 }
