1753d837c2398906128536549ef58b659c8c4efe
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-03 18:51:34 -0300

    cambio etiqueta a por link

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,34 +1,36 @@
 import React from "react"
 import "./Header.css"
+import {Link} from "react-router-dom"
+
 const Navbar = () => {
     return (
         <>
             <header className="header">
                 <nav className = "navbar navbar-expand-lg navbar-dark bg-primary">
                     <div className="container-fluid">
-                        <a class="navbar-brand" href="/">Inicio(img)</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropDown" aria-controls="navbarNavDropDown" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                        <Link className="navbar-brand" to="/">Inicio(img)</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropDown" aria-controls="navbarNavDropDown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNavDropDown">
                             <ul className="navbar-nav" id="navbarNav">
                                 <li className="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle navbar-brand" href="#" id="dropdown-products" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</a>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdown-products">
-                                        <li><a class="dropdown-item" href="/juegos">Juegos</a></li>
-                                        <li><a class="dropdown-item" href="/perifericos">Perifericos</a></li>
-                                        <li><a class="dropdown-item" href="/gabinetes">Gabinetes</a></li>
-                                        <li><a class="dropdown-item" href="/sillas-gamer">Sillas Gamer</a></li>
+                                    <Link className="nav-link dropdown-toggle navbar-brand" to="#" id="dropdown-products" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</Link>
+                                    <ul className="dropdown-menu" aria-labelledby="dropdown-products">
+                                        <li><Link className="dropdown-item" to="/juegos">Juegos</Link></li>
+                                        <li><Link className="dropdown-item" to="/perifericos">Perifericos</Link></li>
+                                        <li><Link className="dropdown-item" to="/gabinetes">Gabinetes</Link></li>
+                                        <li><Link className="dropdown-item" to="/sillas-gamer">Sillas Gamer</Link></li>
                                     </ul>
                                 </li>
                                 <li className="nav-item">
-                                    <a href="/contacto" className = "nav-link navbar-brand">Contacto</a>
+                                    <Link to="/contacto" className = "nav-link navbar-brand">Contacto</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a href="/about" className = "nav-link navbar-brand">Sobre nosotros</a>
+                                    <Link to="/about" className = "nav-link navbar-brand">Sobre nosotros</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a href="/login" className = "nav-link navbar-brand">Login</a>
+                                    <Link to="/login" className = "nav-link navbar-brand">Login</Link>
                                 </li>
                             </ul>
                         </div>
