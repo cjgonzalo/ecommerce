452fabc72c6920983fbf871fb67bbeab89cacf21
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-05 23:45:36 -0300

    cambios al footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,18 +3,38 @@ import "./Footer.css"
 const Footer = () => {
     return (
         <footer className="footer">
+            
+            <div className="container">
+                        <p className="text-center"><b>Todos los derechos reservados Mundo Gamer 2021</b></p>
+            </div>
             <div className="row">
-                <div className="col col-sm-6">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, velit. Consequatur laboriosam quos unde reprehenderit molestias quaerat maxime quas architecto! Asperiores nemo dignissimos laborum, quas tempora repellendus aut adipisci nulla?</p>
+                <div className="col-6">
+                    <div className="container text-center">
+                        <span className="text-center"><b>Encontranos en:</b></span>
+                        <ul className="networks list-group list-group-horizontal justify-content-center">
+                            <li className="list-item">
+                                <a href="https://es-la.facebook.com/" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>
+                            </li>
+                            <li className="list-item">
+                                <a href="https://www.instagram.com/?hl=es-la" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+                            </li>
+                            <li className="list-item">
+                                <a href="https://twitter.com/?lang=es" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="col col-sm-6">
-                    <ul>
-                        <li>
-                            <a href="github.com">Github</a>
+                <div className="col-6">    
+                    <span className="text-center"><b>Código del proyecto</b></span>
+                    <ul className="list-group list-group-horizontal justify-content-start">
+                        <li className="list-item">
+                            <a href="https://github.com/cjgonzalo/ecommerce" target="_blank"><i class="fab fa-github fa-2x"></i></a>
                         </li>
                     </ul>
                 </div>
             </div>
+            
+            
         </footer>
     )
 }
