311ae6414ab9c123a73201552738388002501e60
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-11 23:29:53 -0300

    nuevos atributos para usuario y producto

--- server/models/product.js
+++ server/models/product.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose")
-const { Schema } = mongoose
+const { Schema, model } = mongoose
 
 const productSchema = new Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
+    title: {type: String, required: true, trim: true},
+    description: {type: String, required: true, trim: true},
     price: {type: Number, required: true},
-    images: [{type: String, required: true}],
+    images: {type: String, required: true},
+    type: {type: String, required: true, trim: true},
+    featured: {type: Boolean, default: false},
     onSale: {type: Boolean, default: false},
     hasStock: {type: Boolean, default: true}
 })
 
-module.exports = productSchema
+module.exports = model("Product", productSchema)
--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,13 @@
-const {Schema} = require("mongoose")
+const {Schema, model} = require("mongoose")
 const product = require("./product.productSchema")
 
 const userSchema = new Schema({
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
     //paymentMethod: {type: ??},
-    kart: [{type: product, default: []}],
+    kart: {type: Array, default: []},
     admin: {type: Boolean, required: true}
 })
 
-module.exports = userSchema
+module.exports = model("User", userSchema)
