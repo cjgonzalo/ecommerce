819250b82d9f6eff3608a694e56bfcb76a4802b9
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-11 23:18:45 -0300

    instalacion y uso de cors

--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ require("dotenv").config()
 const morgan = require("morgan")
 const path = require("path")
 const { mongoose } = require("./database")
+const cors = require("cors")
 
 // Settings
-app.set("port", process.env.PORT || 3001)
+app.set("port", process.env.PORT || 4000)
 
 // Middlewares
+app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json())
 
@@ -16,12 +18,12 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, ".././client/public/")))
 
 // Routes
-app.use("/", require("./routes/home.routes"))
+app.use("/api/home", require("./routes/home.routes"))
 app.use("/api/login", require("./routes/login.routes"))
 app.use("/api/productos", require("./routes/products.routes"))
-app.use("/contacto", require("./routes/contact.routes"))
+app.use("/api/contacto", require("./routes/contact.routes"))
 app.use((req, res) => {
-    res.status(404).render("404")
+    res.status(404).send("Página no encontrada")
 })
 
 app.listen(app.get("port"), () => {
