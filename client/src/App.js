c6ace59ccf1a797447b9e82a15fc014680546c0d
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-11 23:43:23 -0300

    ruta de pagina de producto

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import About from "./views/about/about"
 import NotFound from "./views/404/404"
 import Footer from "./components/footer/Footer"
 import ProductsPage from "./views/productsPage/productsPage"
+import SingleProduct from "./views/singleProduct/singleProduct"
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
           <Route path = "/contacto" element={<Contact />} />
           <Route path = "/login" element={<Login/>} />
           <Route path = "/about" element={<About/>} />
-          <Route path = "/producto/:id" element={<Contact />} />
+          <Route path = "/producto/:id" element={<SingleProduct />} />
           <Route path = "/juegos" element={<ProductsPage type="games"/>} />
           <Route path = "/perifericos" element={<ProductsPage type="peripherals"/>} />
           <Route path = "/gabinetes" element={<ProductsPage type="cabinets"/>} />
