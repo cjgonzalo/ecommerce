62441e79a19bcc91dd7204262b6bc01e4342e742
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-24 17:41:24 -0300

    empezando la pagina de login

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import "./Login.css"
 
-const login = () => {
+const Login = () => {
     return (
-        <div>
-            <h1>Login view</h1>
+        <div className='justify-content-center d-flex'>
+            <form action="" method="post">
+                <div className="row">
+                    <div className="col-12">
+                        <label htmlFor="username">Usuario</label>
+                        <input type="text" name="username" id="username" className='form-control' placeholder='Ingrese su usuario' />
+                    </div>
+                    <div className="col-12">
+                        <label htmlFor="password">Contraseña</label>
+                        <input type="password" name="password" id="password" className='form-control' placeholder='Ingrese su contraseña' />
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
 
-export default login
+export default Login
