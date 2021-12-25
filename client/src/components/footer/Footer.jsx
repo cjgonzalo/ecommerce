7e779e6271befb29de006398ec3548a1c841abdf
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-25 15:45:39 -0300

    borro columna de footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -5,34 +5,22 @@ const Footer = () => {
         <footer className="footer">
             
             <div className="container">
-                <p className="text-center"><b>Todos los derechos reservados Mundo Gamer 2021</b></p>
+                <p className="text-center">Todos los derechos reservados Mundo Gamer 2021</p>
             </div>
-            <div className="row">
-                <div className="col-6">
-                    <div className="container text-center">
-                        <span className="text-center"><b>Encontranos en:</b></span>
-                        <ul className="networks list-group list-group-horizontal justify-content-center">
-                            <li className="list-item">
-                                <a href="https://es-la.facebook.com/" target="_blank"><i className="fab fa-facebook-square fa-2x"></i></a>
-                            </li>
-                            <li className="list-item">
-                                <a href="https://www.instagram.com/?hl=es-la" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
-                            </li>
-                            <li className="list-item">
-                                <a href="https://twitter.com/?lang=es" target="_blank"><i className="fab fa-twitter fa-2x"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="col-6">    
-                    <span className="text-center"><b>Código del proyecto</b></span>
-                    <ul className="list-group list-group-horizontal justify-content-start">
+                <div className="container text-center">
+                    <span className="text-center">Encontranos en:</span>
+                    <ul className="networks list-group list-group-horizontal justify-content-center">
                         <li className="list-item">
-                            <a href="https://github.com/cjgonzalo/ecommerce" target="_blank"><i className="fab fa-github fa-2x"></i></a>
+                            <a href="https://es-la.facebook.com/" target="_blank"><i className="fab fa-facebook-square fa-2x"></i></a>
+                        </li>
+                        <li className="list-item">
+                            <a href="https://www.instagram.com/?hl=es-la" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
+                        </li>
+                        <li className="list-item">
+                            <a href="https://twitter.com/?lang=es" target="_blank"><i className="fab fa-twitter fa-2x"></i></a>
                         </li>
                     </ul>
                 </div>
-            </div>  
         </footer>
     )
 }
