8dcd2bd39b41e39d67ba4576a9ec07b9b91143aa
cjgonzalo <gonicasanova.gc@gmail.com>
2021-12-25 22:03:41 -0300

    el boton desaparece al estar arriba de todo

--- client/src/components/ScrollTopBtn/ScrollTopBtn.jsx
+++ client/src/components/ScrollTopBtn/ScrollTopBtn.jsx
@@ -1,20 +1,32 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import "./ScrollTopBtn.css"
 
 const ScrollTopBtn = () => {
 
-    // const [visibility, setVisibility] = useState(false)
+    const [visibility, setVisibility] = useState(false)
 
     const scrollToTop = () => {
         document.documentElement.scrollTop = 0
-        setVisibility(false)
     }
 
-    return (
-        <button onClick={scrollToTop} className='scroll-top btn btn-primary'>
-            <i className="fas fa-chevron-up"></i>
-        </button>
-    )
+    useEffect(() => {
+        const updateScrollPos = () => {
+            document.documentElement.scrollTop > 0 ? setVisibility(true) : setVisibility(false)
+        }
+        window.addEventListener("scroll", updateScrollPos)
+
+        return () => {
+            window.removeEventListener("scroll", updateScrollPos)
+        }
+    })
+
+        return visibility ?
+            <button onClick={scrollToTop} className='scroll-top btn btn-primary'>
+                <i className="fas fa-chevron-up"></i>
+            </button>
+        :
+            <></>
+    
 }
 
 export default ScrollTopBtn
