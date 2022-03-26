acb18a8574ccd9e2eeaa948008b8935056833f6d
cjgonzalo <gonicasanova.gc@gmail.com>
2022-03-26 14:35:52 -0300

    subo ultimos cambios para no perderlos

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -3,19 +3,30 @@ import "./Login.css"
 
 const Login = () => {
     return (
-        <div className='justify-content-center d-flex'>
-            <form action="" method="post">
-                <div className="row">
-                    <div className="col-12">
-                        <label htmlFor="username">Usuario</label>
-                        <input type="text" name="username" id="username" className='form-control' placeholder='Ingrese su usuario' />
+        <div className="login-form-container container">
+            <ul class="nav nav-tabs justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Active</a>
+                    <form action="" method="post">
+                    <div className="row">
+                        <div className="col-12">
+                            <label htmlFor="username">Usuario</label>
+                            <input type="text" name="username" id="username" className='form-control' placeholder='Ingrese su usuario' />
+                        </div>
+                        <div className="col-12">
+                            <label htmlFor="password">Contraseña</label>
+                            <input type="password" name="password" id="password" className='form-control' placeholder='Ingrese su contraseña' />
+                        </div>
                     </div>
-                    <div className="col-12">
-                        <label htmlFor="password">Contraseña</label>
-                        <input type="password" name="password" id="password" className='form-control' placeholder='Ingrese su contraseña' />
-                    </div>
-                </div>
-            </form>
+                </form>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#" role="button">Link</a>
+                </li>
+            </ul>
+            {/* <div className='justify-content-center d-flex'>
+                
+            </div> */}
         </div>
     )
 }
